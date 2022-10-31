87cc391bb63a510af8c89267432f8ac268a9ddba
sehyoungchoi <hope69034@gmail.com>
2022-10-31 21:44:00 +0900

    1111

---
+++ eclipse/client/src/components/Calendar.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import FullCalendar from '@fullcalendar/react';
+import dayGridPlugin from '@fullcalendar/daygrid';
+import interactionPlugin from "@fullcalendar/interaction";
+
+export default class MyCalendar extends React.Component {
+
+
+  render() {
+    return(
+      <div style={{width: "70%", margin:"auto", marginTop:"3%"}}>
+        <FullCalendar
+          defaultView="dayGridMonth"
+          plugins={[ dayGridPlugin, interactionPlugin ]}
+          initialView={"dayGridMonth"}
+          weekends={true}
+          events={[
+            { title: '바디워시,치약구매', date: '2022-10-21' },
+            { title: '치과 검진 가기', date: '2022-10-20' },
+            { title: '프로젝트 회의', date: '2022-10-20' }
+          ]}
+        />
+      </div>
+    );
+  };
+}
